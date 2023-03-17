fec5223d26385dafb8471b84f7307e95ccbe066e
Isheanesu Valentine Zvikaramba <zvkish001@myuct.ac.za>
2023-03-17 13:28:11 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,55 @@
+// Description----------------------------------------------------------------|
+/*
+ *
+ */
+// DEFINES AND INCLUDES-------------------------------------------------------|
+
+#define STM32F051												   //COMPULSORY
+#include "stm32f0xx.h"											   //COMPULSORY
+#include "lcd_stm32f0.h"
+#include <stdio.h>
+
+// GLOBAL VARIABLES ----------------------------------------------------------|
+
+
+
+
+// FUNCTION DECLARATIONS -----------------------------------------------------|
+
+void main(void);
+void display_on_LCD(uint8_t num);
+void init_LEDs(void);
+
+// MAIN FUNCTION -------------------------------------------------------------|
+
+void main(void)
+{
+
+
+	while(1)
+	{
+
+	}
+}
+// OTHER FUNCTIONS -----------------------------------------------------------|
+
+void display_on_LCD(uint8_t num){ //function to intiate LED
+	char str[3];
+	sprintf(str,"%d",num);
+	lcd_putstring(str);
+}
+
+void init_LEDs(void){
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	GPIOB->MODER |= (
+				 GPIO_MODER_MODER0_0|
+				 GPIO_MODER_MODER1_0|
+				 GPIO_MODER_MODER2_0|
+				 GPIO_MODER_MODER3_0|
+				 GPIO_MODER_MODER4_0|
+				 GPIO_MODER_MODER5_0|
+				 GPIO_MODER_MODER6_0|
+				 GPIO_MODER_MODER7_0);
+}
+
+
